59d5b42a6512e42faef60265aaa4453dad5be31f
Melody424 <melodywang0424@gmail.com>
2025-06-01 19:16:09 +0000

    0.02.5.1

--- pages/test.py
+++ pages/test.py
@@ -7,37 +7,6 @@ from coding.persona_tools import persona_name, persona_title, get_clean_titles, 
 import pandas as pd
 
 
-
-
-def make_persona(image_path, output_path = "pages/persona_{name}.png"):
-    job_titles = df["Job Title"].dropna().tolist()
-    cleaned_raw = get_clean_titles(job_titles)
-    print(format_titles_centered(cleaned_raw))
-
-    img, name = persona_name(image_path)
-    img = persona_title(img, cleaned_raw)
-
-
-    if img is None:
-        return  # 尚未輸入名字，提早結束
-
-
-    # 將 PIL 圖片轉成 BytesIO
-    img_bytes = BytesIO()
-    img.save(img_bytes, format="PNG")
-    img_bytes.seek(0)
-
-    # 顯示圖片
-    st.image(img_bytes, caption="你的個人化 Persona", use_container_width=True)
-
-    # 提供下載按鈕（從記憶體中下載）
-    st.download_button(
-        label="⬇️ Download Your Persona",
-        data=img_bytes,
-        file_name=f"persona_{name}.png",
-        mime="image/png"
-    )
-
 def save_lang():
     st.session_state["lang_setting"] = st.session_state.get("language_select")
 
